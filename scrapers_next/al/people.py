2b8b18974222e5ab84220ea81ab5b2d3aa28ed3d
Fox Seligman <foxseligman@gmail.com>
2025-11-27 12:07:02 -0500

    Fix AL people scraper

--- scrapers_next/al/people.py
+++ scrapers_next/al/people.py
@@ -1,93 +1,16 @@
-from spatula import JsonPage, URL
-from openstates.models import ScrapePerson
 import json
+from pathlib import Path
+import typing
+
+from spatula.pages import JsonListPage, SkipItem
+from spatula.sources import URL
+
+from openstates.models import ScrapePerson
+from openstates.models.people import PartyName, RoleType
+from openstates.utils.scrape import read_gql_file
 
 
-class MemberList(JsonPage):
-    def process_page(self):
-        data = self.response.json().get("data")
-
-        # Combine all data fields that contain a member object
-        members = []
-        member_fields = [
-            "houseMembersNewImgUrl",
-            "houseLeadersNewImgUrl",
-            "senateMembersNewImgUrl",
-            "senateLeadersNewImgUrl",
-        ]
-        for field in member_fields:
-            # Ignore fields that are missing
-            if data.get(field):
-                members += data.get(field)
-
-        for member in members:
-            # Skip adding member if district info isn't found.
-            if not member.get("District") or member.get("District") == "Not Available":
-                name = member.get("FullName")
-                self.logger.warning(f"{name} has no listed district, skipping.")
-                continue
-
-            # Remove extra text from district
-            district = member.get("District").split(" ")[-1]
-
-            if "zzz" in member.get("FullName"):
-                self.logger.warning(f"{district} is vacant, skipping.")
-                continue
-
-            p = ScrapePerson(
-                name=member.get("FullName"),
-                state="al",
-                chamber=self.chamber,
-                party=member.get("Affiliation"),
-                district=district,
-                email=member.get("Email"),
-                image=member.get("NewImgUrl"),
-                given_name=member.get("FirstName"),
-                family_name=member.get("LastName"),
-            )
-
-            p.add_source(
-                self.source.url, note="graphql endpoint for member information"
-            )
-
-            # Member profiles can't be access by url alone, so add homepage links
-            # for house/senate list page instead
-            if self.chamber == "upper":
-                # Senate member list
-                p.add_link(
-                    "https://alison.legislature.state.al.us/senate-leaders-members",
-                    note="homepage",
-                )
-            elif self.chamber == "lower":
-                # House member list
-                p.add_link(
-                    "https://alison.legislature.state.al.us/house-leaders-members",
-                    note="homepage",
-                )
-
-            # Missing data may sometimes be set to a string like "None Listed"
-            capitol_address = member.get("FullAddress")
-            if not capitol_address.startswith("No Address Listed"):
-                p.capitol_office.address = capitol_address.strip().replace("\n", ",")
-
-            capitol_phone = member.get("Phone")
-            if capitol_phone and not capitol_phone == "None Listed":
-                p.capitol_office.voice = capitol_phone
-
-            district_address = member.get("FullDistrictAddress")
-            if not district_address.startswith("No District Address Listed"):
-                p.district_office.address = district_address.strip().replace("\n", ",")
-
-            district_phone = member.get("DistrictPhone")
-            if district_phone and not district_phone == "None Listed":
-                p.district_office.voice = district_phone
-
-            p.extras["counties"] = member.get("Counties")
-
-            yield p
-
-
-def graphql_query(data):
+def graphql_query(data) -> URL:
     return URL(
         "https://gql.api.alison.legislature.state.al.us/graphql",
         method="POST",
@@ -96,36 +19,111 @@ def graphql_query(data):
             # Referer required or graphql will respond with http error 403
             "Referer": "https://alison.legislature.state.al.us/",
         },
-        data=json.dumps(data),
+        data=json.dumps(data), # type: ignore - str is allowed underneath
     )
 
 
-def get_members_source(chamber):
-    chamber_type = "house" if chamber == "lower" else "senate"
-    fields = "Affiliation,FullName,FirstName,LastName,NewImgUrl,District,Counties,FullAddress,Phone,Email,FullDistrictAddress,DistrictOffice,Fax,DistrictFax,DistrictPhone,DistrictEmail"
-    # Need to check both Members and Leaders
+def get_members_source() -> URL:
     return graphql_query(
         {
-            "query": "{"
-            + chamber_type
-            + "MembersNewImgUrl{"
-            + fields
-            + "} "
-            + chamber_type
-            + "LeadersNewImgUrl{"
-            + fields
-            + "}}",
-            "operationName": "",
-            "variables": [],
+            "operationName": "legislativeMembers",
+            "query": read_gql_file(Path(__file__).parent / "people.gql"),
+            "variables": {},
         },
     )
 
 
-class RepList(MemberList):
-    chamber = "lower"
-    source = get_members_source(chamber="lower")
+class MemberList(JsonListPage):
+    source = get_members_source()
 
+    def process_page(self) -> typing.Iterable[typing.Any]:
+        yield from self._process_or_skip_loop(self.data["data"]["legislativeMembers"]["data"])
 
-class SenList(MemberList):
-    chamber = "upper"
-    source = get_members_source(chamber="upper")
+    def process_item(self, item):
+        # Skip adding member if district info isn't found.
+        if item["district"] == "Not Available":
+            name = item["fullName"]
+            raise SkipItem(f"{name} has no listed district, skipping.")
+
+        # Use just the district number
+        district = item["district"].split(" ")[-1]
+
+        if item["fullName"] == "Vacant":
+            raise SkipItem(f"{district} is vacant, skipping.")
+
+        # The API's Afilliation enum currently only has "D" and "R".
+        # This should fail as we learn of other possible values.
+        party = {
+            "D": PartyName.DEM,
+            "R": PartyName.REP
+        }[item["affiliation"]]
+
+        chamber = {
+            "House": RoleType.LOWER,
+            "Senate": RoleType.UPPER,
+        }[item["body"]]
+
+        p = ScrapePerson(
+            name=item["fullName"],
+            state="al",
+            party=party,
+            district=district,
+            chamber=chamber,
+            image=item["imageUrl"],
+            email=item["email"] or "",
+            given_name=item["firstName"],
+            family_name=item["lastName"],
+        )
+
+        p.add_source(
+            str(self.source),
+            note="graphql endpoint for member information"
+        )
+
+        # Member profiles can't be access by url alone, so add homepage links
+        # for house/senate list page instead
+        if chamber == RoleType.UPPER:
+            # Senate member list
+            p.add_link(
+                "https://alison.legislature.state.al.us/senate-leaders-members",
+                note="homepage",
+            )
+        elif chamber == RoleType.LOWER:
+            # House member list
+            p.add_link(
+                "https://alison.legislature.state.al.us/house-leaders-members",
+                note="homepage",
+            )
+
+        # Missing data may sometimes be set to a string like "None Listed"
+        capitol_address = item["fullAddress"]
+        if not capitol_address.startswith("No Address Listed"):
+            p.capitol_office.address = capitol_address.strip().replace("\n", ", ")
+
+        capitol_phone = item["phone"]
+        if capitol_phone and not capitol_phone == "None Listed":
+            p.capitol_office.voice = capitol_phone
+
+        capitol_fax = item["fax"]
+        if capitol_fax:
+            p.capitol_office.fax = capitol_fax
+
+        district_address = item["fullDistrictAddress"]
+        if not district_address.startswith("No District Address Listed"):
+            p.district_office.address = district_address.strip().replace("\n", ", ")
+
+        district_phone = item["districtPhone"]
+        if district_phone and not district_phone == "None Listed":
+            p.district_office.voice = district_phone
+
+        district_fax = item["districtFax"]
+        if district_fax:
+            p.district_office.fax = district_fax
+
+        p.extras["counties"] = item["counties"]
+
+        leadership_title = item["leadershipTitle"]
+        if leadership_title:
+            p.extras["title"] = leadership_title
+
+        return p
