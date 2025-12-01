af6fa857d5232d2f0f742f88bb1706473c71ea9b
Fox Seligman <foxseligman@gmail.com>
2025-11-30 22:26:21 -0500

    Improve AR people scraper by scraping mostly JSON

--- scrapers_next/ar/people.py
+++ scrapers_next/ar/people.py
@@ -1,133 +1,125 @@
-import attr
-from spatula import HtmlListPage, HtmlPage, XPath, CSS, SelectorError
+from spatula.pages import JsonListPage, HtmlPage, SkipItem
+from spatula.selectors import CSS, XPath
+
 from openstates.models import ScrapePerson
+from openstates.models.people import PartyName, RoleType
+from openstates.utils.scrape import infer_name_parts
 
 
-@attr.s(auto_attribs=True)
-class PartialMember:
-    name: str
-    url: str
-    chamber: str = ""
-
-
-class LegDetail(HtmlPage):
-    example_source = (
-        "https://www.arkleg.state.ar.us/Legislators/Detail?"
-        "member=B.+Ballinger&ddBienniumSession=2023%2F2023R"
-    )
-
+class ExtraDetail(HtmlPage):
     def process_page(self):
+        subtitle = CSS("#content h1 + *").match_one(self.root).text_content().strip()
+        if "Deceased" in subtitle:
+            raise SkipItem("Member is deceased")
 
-        table_div = CSS(".col-md-7").match(self.root)[0]
+        if "Resigned" in subtitle:
+            raise SkipItem("Member has resigned")
 
-        rows = CSS(".row").match(table_div)
+        biography_link = XPath(
+            ".//div[@id='tableDataWrapper']"
+            "//div[contains(@class, 'row')]"
+            "//div[normalize-space(.)='Biography:']"
+            "//following-sibling::div[1]//a/@href"
+        ).match(self.root, min_items=0)
+        if biography_link:
+            self.input.extras["biography"] = biography_link[0]
 
-        party = ""
-        name_party = CSS("h1").match_one(self.root).text_content()
+        return self.input
 
-        if name_party.endswith("(R)"):
-            party = "Republican"
-        elif name_party.endswith("(D)"):
-            party = "Democrat"
-        elif name_party.endswith("(I)"):
-            party = "Independent"
-        elif name_party.endswith("(G)"):
-            party = "Green"
 
-        image = CSS(".MemberPhoto").match_one(self.root).get("src")
+class PeopleList(JsonListPage):
+    chamber: RoleType
 
-        table = {
-            "Phone:": "",
-            "Email:": "",
-            "District:": "",
-            "Seniority:": "",
-            "Occupation:": "",
-            "Church Affiliation:": "",
-            "Veteran:": "",
-            "Public Service:": "",
-            "Biography:": "",
-        }
+    def get_source_from_input(self):
+        chamber = {
+            RoleType.UPPER: "Senate",
+            RoleType.LOWER: "House",
+        }[self.chamber]
 
-        for row in rows:
-            try:
-                type = CSS(".col-md-3").match_one(row).text_content()
-                info = CSS(".col-md-9").match_one(row).text_content().strip()
-                if type == "Biography:":
-                    info = CSS(".col-md-9 a").match_one(row).get("href")
-                if type in table:
-                    table[type] = info
-            except SelectorError:
-                pass
-
-        p = ScrapePerson(
-            name=self.input.name,
-            state="ar",
-            chamber=self.input.chamber,
-            party=party,
-            image=image,
-            district=table["District:"],
-            email=table["Email:"],
+        return (
+            "https://services2.arcgis.com/wu7u046EJ59dJv3h/arcgis/rest/services/"
+            f"Arkansas%20State%20{chamber}/FeatureServer/0/query"
+            "?f=json&outFields=*&spatialRel=esriSpatialRelIntersects&where=1%20=%201"
         )
 
-        if table["Biography:"] != "":
-            p.add_link(table["Biography:"], "Biography")
+    def postprocess_response(self):
+        super().postprocess_response()
+        self.data = self.data["features"]
 
-        for key in table:
-            if (
-                key == "Phone:"
-                or key == "Email:"
-                or key == "District:"
-                or key == "Biography:"
-            ):
-                continue
-            elif table[key] != "":
-                # remove the colon at the end
-                p.extras[key[:-1].lower()] = table[key]
-        try:
-            address = CSS(".col-md-12 p b").match_one(self.root).text_content()
-            full_address = address[:-5] + "AR " + address[-5:]
-            p.district_office.address = full_address
-        except SelectorError:
-            pass
+    def process_item(self, item):
+        attributes = item["attributes"]
 
-        p.add_source(self.source.url)
-        p.add_source(self.input.url)
+        name = " ".join([attributes["FirstName"], attributes["LastName"]])
+
+        party = {
+            "D": PartyName.DEM,
+            "R": PartyName.REP,
+            "G": PartyName.GREEN,
+            "I": PartyName.IND,
+        }.get(attributes["Party"])
+        if not party:
+            raise SkipItem(f"Unknown party abbreviation: {attributes['Party']}")
+
+        chamber = {
+            "Senate": RoleType.UPPER,
+            "House": RoleType.LOWER,
+        }.get(attributes["Chamber"])
+        if not chamber:
+            raise SkipItem(f"Unknown chamber title: {attributes['Chamber']}")
+
+        p = ScrapePerson(
+            name=name,
+            state="ar",
+            party=party,
+            district=attributes["district"],
+            chamber=chamber,
+            image=attributes["PhotoURL"],
+            email=attributes["Email"],
+            # given_name=attributes["FirstName"],  # Includes middle name/initial and sometimes a prefix
+            # family_name=attributes["LastName"],  # Includes suffixes
+            # suffix="",  # No suffix info available separately
+        )
+        infer_name_parts(p)
+
+        p.add_source(str(self.source), "member list JSON")
+        p.add_source(attributes["MemberURL"], "member detail page")
+
+        p.add_link("https://www.arkleg.state.ar.us/Legislators/List", "member list page")
+        p.add_link(attributes["MemberURL"], "member detail page")
+        member_source = (
+            "https://services2.arcgis.com/wu7u046EJ59dJv3h/arcgis/rest/services/"
+            f"Arkansas%20State%20{attributes['Chamber']}/FeatureServer/0/query"
+            f"?f=json&outFields=*&where=districtn={attributes['district']}"
+        )
+        p.add_link(member_source, "member detail JSON")
+
+        # Individual rooms aren't listed; using general capitol address
+        p.capitol_office.address = "1 Capitol Mall, Fifth Floor, Little Rock, AR 72201"
+
+        if attributes["Address"]:
+            p.district_office.address = f"{attributes['Address']}, {attributes['City']}, AR {attributes['Zip']}"
+        p.district_office.voice = attributes["Phone"]
+
+        # Extras
+        if attributes["Senior"]:
+            p.extras["seniority"] = attributes["Senior"]
+        if attributes["Occupation"]:
+            p.extras["occupation"] = attributes["Occupation"]
+        if attributes["ChurchAffiliation"]:
+            p.extras["church"] = attributes["ChurchAffiliation"]
+        if attributes["PublicService"]:
+            p.extras["public_service"] = attributes["PublicService"]
+        if attributes["Veteran"]:
+            p.extras["veteran"] = True if attributes["Veteran"].lower() == "yes" else False
+
+        p = ExtraDetail(p, source=attributes["MemberURL"])
 
         return p
 
 
-class LegList(HtmlListPage):
-    source = (
-        "https://www.arkleg.state.ar.us/Legislators/List?"
-        "sort=Type&by=desc&ddBienniumSession=2023%2F2023R#SearchResults"
-    )
-    selector = XPath(
-        "//div[@role='grid']"
-        "//div[contains(@class, 'row')]"
-        "//div[@class='col-md-6']"
-    )
+class SenatorList(PeopleList):
+    chamber = RoleType.UPPER
 
-    def process_item(self, item):
-        chamber_name = (
-            item.text_content()
-            .replace("\r\n", "")
-            .strip()
-            .replace("                                        ", "   ")
-            .split("   ")
-        )
 
-        chamber = chamber_name[0]
-
-        name = chamber_name[1].replace("  ", " ")
-        if "resigned" in name.lower():
-            self.skip()
-
-        if chamber == "Senator":
-            chamber = "upper"
-        elif chamber == "Representative":
-            chamber = "lower"
-
-        source = item.getchildren()[0].get("href")
-        p = PartialMember(name=name, chamber=chamber, url=self.source.url)
-
-        return LegDetail(p, source=source)
+class RepresentativeList(PeopleList):
+    chamber = RoleType.LOWER
